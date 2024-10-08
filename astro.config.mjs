3dd92ca8c48ef988ba2004fca80189299b0515ac
agungbharata <afyastudio@gmail.com>
2024-10-08 13:33:52 +0700

    new upload docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import tailwind from '@astrojs/tailwind';
-
-import alpinejs from '@astrojs/alpinejs';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import tailwind from "@astrojs/tailwind";
+import image from "@astrojs/image";
+import alpinejs from "@astrojs/alpinejs";
 
 export default defineConfig({
-  integrations: [mdx(), tailwind(), alpinejs()],
+  integrations: [
+    mdx(),
+    tailwind(),
+    alpinejs(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp", // Menggunakan loader Sharp untuk optimasi gambar
+    }),
+  ],
 });
-
-
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,18 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
       fontFamily: {
-        'sans': ['Manrope', ...defaultTheme.fontFamily.sans],
-        'sora': ['Sora', ...defaultTheme.fontFamily.sans],
+        sans: ["Manrope", ...defaultTheme.fontFamily.sans],
+        sora: ["Sora", ...defaultTheme.fontFamily.sans],
+      },
+      lineHeight: {
+        "custom-3em": "3em", // Custom line-height value
       },
     },
   },
-  plugins: [
-    require('@tailwindcss/typography'),
-  ],
-  darkMode: 'class',
-}
+  plugins: [require("@tailwindcss/typography")],
+  darkMode: "class",
+};
